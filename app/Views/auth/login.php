3c65d2992c29169bf2b275ef003bfb6e151635df
kokurate <ericksanger66@gmail.com>
2021-06-13 20:18:48 +0800

    Menyesuaikan fungsi dalam login myth/auth dengan siplakom

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -27,6 +27,10 @@
     <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>/login/css/util.css">
     <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>/login/css/main.css">
     <!--===============================================================================================-->
+
+    <!-- bootstrap 4 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
 </head>
 
 <body>
@@ -38,22 +42,58 @@
                 <span class="login100-form-title p-b-41">
                     SIPLAKOM Login
                 </span>
-                <form class="login100-form validate-form p-b-33 p-t-5">
 
-                    <div class="wrap-input100 validate-input" data-validate="Enter username">
-                        <input class="input100" type="text" name="username" placeholder="User name">
-                        <span class="focus-input100" data-placeholder="&#xe82a;"></span>
-                    </div>
+                <div class="text-center mb-2">
+                    <?= view('Myth\Auth\Views\_message_block') ?>
+                </div>
+
+
+                <form action="<?= route_to('login') ?>" method="post" class="login100-form validate-form p-b-33 p-t-5">
+                    <?= csrf_field() ?>
+
+                    <?php if ($config->validFields === ['email']) : ?>
+                        <div class="wrap-input100 validate-input" data-validate="Enter username">
+                            <input class="input100   <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>" type="email" name="login" placeholder="<?= lang('Auth.email') ?>">
+                            <div class="invalid-feedback">
+                                <?= session('errors.login') ?>
+                            </div>
+
+                            <span class="focus-input100" data-placeholder="&#xe82a;"></span>
+                        </div>
+                    <?php else : ?>
+                        <div class="wrap-input100 validate-input">
+                            <input type="text" class="input100 <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>" name="login" placeholder="<?= lang('Auth.emailOrUsername') ?>">
+                            <div class="invalid-feedback">
+                                <?= session('errors.login') ?>
+                            </div>
+                            <span class="focus-input100" data-placeholder="&#xe82a;"></span>
+                        </div>
+                    <?php endif; ?>
+
+
+                    <!-- <div class="wrap-input100 validate-input" data-validate="Enter password">
+                        <input class="input100" type="password" name="password" placeholder="Password">
+                        <span class="focus-input100" data-placeholder="&#xe80f;"></span>
+                    </div> -->
+
 
                     <div class="wrap-input100 validate-input" data-validate="Enter password">
-                        <input class="input100" type="password" name="password" placeholder="Password">
+
+                        <input type="password" name="password" class="input100  <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" placeholder="<?= lang('Auth.password') ?>">
+                        <div class="invalid-feedback">
+                            <?= session('errors.password') ?>
+                        </div>
                         <span class="focus-input100" data-placeholder="&#xe80f;"></span>
                     </div>
 
+
+                    <br>
+
+
+
+
                     <div class="container-login100-form-btn m-t-32">
-                        <button class="login100-form-btn">
-                            Login
-                        </button>
+                        <button type="submit" class="login100-form-btn"><?= lang('Auth.loginAction') ?></button>
                     </div>
 
                 </form>
